39e1e6e13a8beb15f844f821d35819cf694ad335
Mariana Avila <marianaavilaa64@gmail.com>
2024-03-02 19:35:10 -0500

    initiaal commit

---
+++ lib/quote.dart
@@ -0,0 +1,13 @@
+class Quote{
+
+ String text="";
+ String author="";
+
+ Quote(String author,String text){
+  this.text=text;
+  this.author=author;
+ }
+
+}
+
+//Quote myquote= Quote("Mariana","Avila");
